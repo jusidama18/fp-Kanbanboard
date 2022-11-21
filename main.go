be9d98cca368a95e9fe37570e8a6b3cffb6947ab
DionFauzi <fauzidion46@gmail.com>
2022-11-21 16:01:35 +0700

    add users

---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"os"
+
+	_handler "Kanbanboard/app/delivery"
+	_repository "Kanbanboard/app/repository"
+	_usecase "Kanbanboard/app/usecase"
+
+	"Kanbanboard/config"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	config.StartDB()
+	db := config.GetDBConnection()
+
+	userRepository := _repository.NewUserRepository(db)
+	userUsecase := _usecase.NewUserUsecase(userRepository)
+
+	api := router.Group("/")
+	_handler.NewUserHanlder(api, userUsecase)
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	router.Run(":" + port)
+}
