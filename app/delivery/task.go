f87848c7fbd80029713f05349cb12caf065ae815
jusidama18 <jusidama18@gmail.com>
2022-12-06 22:15:37 +0700

    Update

--- app/delivery/task.go
+++ app/delivery/task.go
@@ -8,6 +8,7 @@ import (
 	"Kanbanboard/domain"
 	"fmt"
 	"net/http"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -20,11 +21,14 @@ func NewTaskController(r *gin.Engine, uc domain.TaskUseCase) {
 	taskCtl := TaskController{
 		uc: uc,
 	}
-
 	taskRouter := r.Group("/tasks")
 	taskRouter.Use(middleware.Authorization([]string{"member", "admin"}))
 	taskRouter.POST("/", taskCtl.CreateTask)
 	taskRouter.GET("/", taskCtl.GetAllTasks)
+	taskRouter.PUT("/:id", taskCtl.PutTask)
+	taskRouter.PATCH("/update-status/:id", taskCtl.PatchTaskStatus)
+	taskRouter.PATCH("/update-category/:id", taskCtl.PatchTaskCategory)
+	taskRouter.DELETE("/:id", taskCtl.DeleteTask)
 }
 
 func (t *TaskController) CreateTask(c *gin.Context) {
@@ -72,4 +76,100 @@ func getUserID(c *gin.Context) (int, error) {
 	}
 	userID := int(id.(float64))
 	return userID, nil
+}
+
+func (t *TaskController) PutTask(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+	var req params.TaskPutByID
+	err = c.ShouldBindJSON(&req)
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+
+	err = helper.ValidateStruct(req)
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+
+	resp, err := t.uc.PutTask(id, req)
+	if err != nil {
+		responses.InternalServerError(c, err.Error())
+		return
+	}
+	responses.Success(c, http.StatusOK, "task update successfully", resp)
+}
+
+func (t *TaskController) PatchTaskStatus(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+	var req params.TaskUpdateStatus
+	err = c.ShouldBindJSON(&req)
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+
+	err = helper.ValidateStruct(req)
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+
+	resp, err := t.uc.PatchTaskStatus(id, req)
+	if err != nil {
+		responses.InternalServerError(c, err.Error())
+		return
+	}
+	responses.Success(c, http.StatusOK, "task update-status patched successfully", resp)
+}
+
+func (t *TaskController) PatchTaskCategory(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+	var req params.TaskUpdateCategory
+	err = c.ShouldBindJSON(&req)
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+
+	err = helper.ValidateStruct(req)
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+
+	resp, err := t.uc.PatchTaskCategory(id, req)
+	if err != nil {
+		responses.InternalServerError(c, err.Error())
+		return
+	}
+	responses.Success(c, http.StatusOK, "task update-category patched successfully", resp)
+}
+
+func (t *TaskController) DeleteTask(c *gin.Context) {
+	id, err := strconv.Atoi(c.Param("id"))
+	if err != nil {
+		responses.BadRequestError(c, err.Error())
+		return
+	}
+
+	err2 := t.uc.DeleteTask(id)
+	if err2 != nil {
+		responses.InternalServerError(c, err.Error())
+		return
+	}
+	responses.Success(c, http.StatusOK, "task deleted successfully", nil)
 }
